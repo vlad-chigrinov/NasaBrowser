9ab99dac5de110171aa78f13b27538bb1531b9b3
FatAlien <vladislav.chigrinov@gmail.com>
2025-04-22 13:46:01 +0300

    Add DI extensions to Application layer

---
+++ NasaBrowser.Application/MediatR/MediartDIExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NasaBrowser.Application.MediatR;
+
+public static class MediartDIExtensions
+{
+    public static void AddAndConfigureMediatR(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+            cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
+        });
+    }
+}
