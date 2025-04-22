286524885376294c75cb026998bfb11073413737
FatAlien <vladislav.chigrinov@gmail.com>
2025-04-22 13:45:31 +0300

    Add MediatR validation behavior

---
+++ NasaBrowser.Application/MediatR/ValidatorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = NasaBrowser.Domain.Exceptions.ValidationException;
+
+namespace NasaBrowser.Application.MediatR
+{
+    public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var failures = _validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures.Select(f => f.ErrorMessage));
+            }
+
+            return await next();
+        }
+    }
+}
